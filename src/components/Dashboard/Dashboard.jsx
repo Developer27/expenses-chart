be6c120533168e0e129cd2b32403d5b3c8401fd5
dmitry <dyivanov32@gmail.com>
2024-04-22 04:10:47 +0300

    second commit

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -170,7 +170,7 @@ export default function Dashboard() {
             <p className='my-balance__price'>₴{myBalance}</p>
           } 
         </div>
-        <img src="../../../assets/logo.svg" alt="balance-icon" className='my_balance__img' />
+        <img src="/logo.svg" alt="balance-icon" className='my_balance__img' />
       </div>
     </div>
       <section className='chart-section'>
